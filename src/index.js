cc1bc76a78940dc5efc8e2cf6d60cd2d6d453e66
pashyaa6255 <76003615+pashyaa6255@users.noreply.github.com>
2021-04-22 00:09:44 +0530

    Font Family Monospace Courier

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 const api = `https://randomuser.me/api`;
 const addUser = document.getElementById("add-user-btn");
-const mainApp = document.getElementById("app");
+//const mainApp = document.getElementById("app");
+const appState = [];
+const userList = document.getElementById("user-list");
+const searchInput = document.getElementById("search");
 addUser.addEventListener("click", async () => {
   const userData = await fetch(api, {
     method: "GET"
   });
   const userJson = await userData.json();
-  console.log(userJson.results[0]);
+  //console.log(userJson.results[0]);
   const user = userJson.results[0];
-  const userEl = document.createElement("click");
-  userEl.innerHTML = `<div>
-  ${user.name.title} ${user.name.first} ${user.name.last}
+  appState.push(user);
+  //console.log(appState);
+  domRenderer(appState);
+  //console.log(appState);
+});
+const domRenderer = (stateArr) => {
+  userList.innerHTML = null;
+  stateArr.forEach((userObj) => {
+    const userEl = document.createElement("div");
+    userEl.innerHTML = `<div>
+  ${userObj.name.title} ${userObj.name.first} ${userObj.name.last}
   </div>`;
-  mainApp.appendChild(userEl);
+    userList.appendChild(userEl);
+  });
+};
+searchInput.addEventListener("keyup", (e) => {
+  console.log(e, searchInput.value);
+  const filteredAppState = appState.filter((user) =>
+    user.name.first.toLowerCase().includes(searchInput.value.toLowerCase())
+  );
+  domRenderer(filteredAppState);
 });
