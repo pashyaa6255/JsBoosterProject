6f771e3fb33dc3d5f78cd63f70a61b782ee5a2de
pashyaa6255 <76003615+pashyaa6255@users.noreply.github.com>
2021-04-23 22:19:07 +0530

    change(SecurityError) workingWithoutSecurityErr

--- src/index.js
+++ src/index.js
@@ -1,35 +1,70 @@
 const api = `https://randomuser.me/api`;
 const addUser = document.getElementById("add-user-btn");
-//const mainApp = document.getElementById("app");
+const sortBtn = document.getElementById("sort");
+const sortBtnDesc = document.getElementById("sortDesc");
+const sortBtnAsc = document.getElementById("sortAsc");
 const appState = [];
 const userList = document.getElementById("user-list");
 const searchInput = document.getElementById("search");
+
+class User {
+  constructor(title, firstname, lastname, gender, email) {
+    this.title = `${title}`;
+    this.name = `${firstname} ${lastname}`;
+    this.email = email;
+    this.gender = gender;
+  }
+}
+
 addUser.addEventListener("click", async () => {
   const userData = await fetch(api, {
     method: "GET"
   });
   const userJson = await userData.json();
-  //console.log(userJson.results[0]);
   const user = userJson.results[0];
+  const classUser = new User(
+    user.name.title,
+    user.name.first,
+    user.name.last,
+    user.gender,
+    user.email
+  );
   appState.push(user);
-  //console.log(appState);
   domRenderer(appState);
-  //console.log(appState);
 });
+
 const domRenderer = (stateArr) => {
   userList.innerHTML = null;
   stateArr.forEach((userObj) => {
     const userEl = document.createElement("div");
     userEl.innerHTML = `<div>
-  ${userObj.name.title} ${userObj.name.first} ${userObj.name.last}
+  <ol>
+    Name:-${userObj.name.title} ${userObj.name.first} ${userObj.name.last} 
+    <li>Gender- ${userObj.gender}</li>
+    <li>EmailId-${userObj.email}</li>
+  </ol>
   </div>`;
     userList.appendChild(userEl);
   });
 };
+
 searchInput.addEventListener("keyup", (e) => {
-  console.log(e, searchInput.value);
-  const filteredAppState = appState.filter((user) =>
-    user.name.first.toLowerCase().includes(searchInput.value.toLowerCase())
+  //console.log(e, searchInput.value);
+  const filteredAppState = appState.filter(
+    (user) =>
+      user.name.first.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.gender.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchInput.value.toLowerCase())
   );
   domRenderer(filteredAppState);
+});
+sortBtnDesc.addEventListener("click", () => {
+  const appStateCopy = [...appState];
+  appStateCopy.sort((a, b) => (a.name.first > b.name.first ? -1 : 1));
+  domRenderer(appStateCopy);
+});
+sortBtnAsc.addEventListener("click", () => {
+  const appStateCopy = [...appState];
+  appStateCopy.sort((a, b) => (a.name.first > b.name.first ? 1 : -1));
+  domRenderer(appStateCopy);
 });
